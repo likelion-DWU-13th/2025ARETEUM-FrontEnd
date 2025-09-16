caf76428b5097549391eb7230288e6784881e2b4
kniiiiko <dywjd1128@naver.com>
2025-09-17 00:19:31 +0900

    typo: 오타 수정

--- areteum_fe/src/pages/Booth-Intro/Intro.jsx
+++ areteum_fe/src/pages/Booth-Intro/Intro.jsx
@@ -6,12 +6,12 @@ const Intro = () => {
   return (
     <I.Container>
       <I.Header>
-        <img id="back" src={`${process.env.PUBLIC_URL}/images/back.png`} alt="scrap" />
+        <img id="back" src={`${process.env.PUBLIC_URL}/images/back.png`} alt="back" />
         <I.Title>즐겨보솜</I.Title>
         <img id="scrap" src={`${process.env.PUBLIC_URL}/images/scrap.png`} alt="scrap" />
       </I.Header>
       <I.Nav>
-        <img id="back" src={`${process.env.PUBLIC_URL}/images/footer.png`} alt="scrap" />
+        <img id="footer" src={`${process.env.PUBLIC_URL}/images/footer.png`} alt="footer" />
       </I.Nav>
     </I.Container>
   );
