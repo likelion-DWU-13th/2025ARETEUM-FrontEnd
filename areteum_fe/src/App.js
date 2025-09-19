2cde3bb55a09d3f866a94360548804a0c8ce31b1
hayeoniill <lhy5441@naver.com>
2025-09-19 12:45:09 +0900

    메인페이지

--- areteum_fe/src/App.js
+++ areteum_fe/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import GlobalStyle from "./pages/GlobalStyles";
 import Intro from "./pages/Booth-Intro/Intro";
 import Detail from "./pages/Booth-Intro/Description";
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
+        <Route path="/mainPage" element={<MainPage />} />
         <Route path="/booth" element={<Intro />} />
         <Route path="/booth-detail" element={<Detail />} />
       </Routes>
