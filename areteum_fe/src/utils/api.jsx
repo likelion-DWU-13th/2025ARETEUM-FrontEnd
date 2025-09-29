1ad2071f7d0b2f2ecb4a07b9d94e47a025cea9f4
yejinkyo <p2006655@gmail.com>
2025-09-29 18:01:55 +0900

    fix: api 경로 수정

--- areteum_fe/src/utils/api.jsx
+++ areteum_fe/src/utils/api.jsx
@@ -5,8 +5,11 @@
  * @returns {Promise<object | null} 부스 객체 or null
  */
 export const getBoothData = async (boothId) => {
+
+    const API_BASE = "https://dev.dwu-festival2025.com:8443"
+
     try {
-        const res = await fetch(`/api/booths/${boothId}`);
+        const res = await fetch(`${API_BASE}/api/booths/${boothId}`);
         
         if (!res.ok) {
             console.warn(`API 응답 오류: ${res.status} - ID: ${boothId}`);
